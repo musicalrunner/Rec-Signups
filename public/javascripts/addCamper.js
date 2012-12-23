b698e1c86240e0f1b2348e50951cb24ef3926d55
Sam <therunner05@gmail.com>
2012-12-22 22:18:25 -0500

    moved add camper form attributes to addCamper.jade

--- public/javascripts/addCamper.js
+++ public/javascripts/addCamper.js
@@ -1,8 +1,8 @@
 $('document').ready(function() {
 
   $('#submit').click(function() {
-    $('#addCamperForm').attr('action', '/setup/addingCamper');
-    $('#addCamperForm').attr('method', 'POST');
+    /*$('#addCamperForm').attr('action', '/setup/addingCamper');
+    $('#addCamperForm').attr('method', 'POST');*/
     $('#addCamperForm').submit();
   });
 });
