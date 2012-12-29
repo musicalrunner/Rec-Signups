22970e279cf93049b3c8d93b4cb00ebe69ce6a57
Sam <therunner05@gmail.com>
2012-12-29 13:42:56 -0500

    Testing local storage to save week number

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,14 +8,22 @@ $('document').ready(function() {
   $('#reset').wrap('<a href="/reset" />');
   */
 
+  $('.button').each(function(index, element) {
+    $element = $(element);
+    $element.wrap('<a href="/' + $element.attr('id') + '" />');
+  });
 
   $('.button').click(function() {
     clicked = $(this);
     var weekNum = $('select#weekID').val();
+    localStorage.setItem('weekNum',weekNum);
+    alert('set local storage');
+    /*
     console.log('weekNum = ' + weekNum);
     console.log('this.id() = ' + clicked.attr('id'));
     $('#weekForm').attr('action', clicked.attr('id')) // finish this!
     $('#weekForm').submit();
+    */
   });
 
 
